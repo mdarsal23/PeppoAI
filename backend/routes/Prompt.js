5b40b79d9b3c6ce2d3544760d365ba8b31a13463
Md Arsal Asif <mdarsalasif@gmail.com>
2025-08-17 04:47:04 +0530

    backend gone

--- backend/routes/Prompt.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-const router = express.Router({ mergeParams: true });
-import  textToVideoController  from '../controller/prompt.js';
-router.get("/give", async (req, res) => {
-    textToVideoController(req, res);
-});
-export default router;
